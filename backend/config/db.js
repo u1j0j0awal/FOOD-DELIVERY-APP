402602c7f2b00b6ce7467028ebe54959181c9147
ujjawal <ujjawalsaurav726@gmail.com>
2025-01-24 01:25:09 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,4 @@
 import mongoose from "mongoose";
-export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://vikas_gaur:12345@cluster0.okqemjq.mongodb.net/food-del').then(console.log("DB connected successfully"))
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://umeshc0509:6bqHmOfPOKf3nf6y@cluster0.cao1k.mongodb.net/food-del').then(console.log("DB connected successfully"))
 }
-
- 
--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,36 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import "dotenv/config"
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
 // app config
-const app = express()
-const port = 4000
+const app = express();
+const port = process.env.PORT || 4000; // Updated to use environment variable
 
 // middleware => request is parsed
-app.use(express.json())
+app.use(express.json());
 // using this we can access backend from frontend
-app.use(cors())
+app.use(cors());
 
 // db connection
 connectDB();
 
 // api endpoints
-app.use("/api/food",foodRouter)
-app.use("/image",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/food", foodRouter);
+app.use("/image", express.static('uploads'));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
-app.get("/",(req, res)=>{
-
-    res.send("API working")
-})
+app.get("/", (req, res) => {
+    res.send("API working");
+});
 
 // running the express server
-app.listen(port,()=>{
-    console.log(`server started on http://localhost:${port}`)
-})
+app.listen(port, () => {
+    console.log(`server started on http://localhost:${port}`);
+});
